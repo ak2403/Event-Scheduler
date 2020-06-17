13405d856c7684ebf82adfca3256dbf9985c1e85
Arun Kumar <arunpasupathi24@gmail.com>
2020-06-17 19:19:31 +1000

    Added IAM Roles and updated the events function.

--- src/handlers/createEvent.js
+++ src/handlers/createEvent.js
@@ -1,7 +1,30 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createEvent(event, context) {
+  let { name, venue, time, capacity } = JSON.parse(event.body);
+
+  let time_now = new Date();
+
+  let newEvent = {
+    id: uuid(),
+    created_on: time_now.toISOString(),
+    name,
+    venue,
+    time,
+    capacity
+  };
+
+  await dynamodb.put({
+    TableName: 'EventsTable',
+    Item: newEvent
+  }).promise();
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+    statusCode: 201,
+    body: JSON.stringify(newEvent),
   };
 }
 
